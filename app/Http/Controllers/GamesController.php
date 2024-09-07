455e308c1532f7695854aa7cdd0364d49d2c38fd
Dan LaFreniere <lafreniere.dj@gmail.com>
2024-09-06 21:39:17 -0700

    Dynamic front page

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
@@ -20,10 +22,27 @@ class GamesController extends Controller
             }
             $popularGames = $this->fetchPopularGames($credentials['access_token']);
             if (!isset($popularGames)) {
-                throw new \Exception('Failed to fetch popular games');
+                throw new \Exception('Failed to fetch popular games.');
             }
-            dd($popularGames);
-        //     return view('games.index', ['games' => $popularGames]);
+            $recentlyReviewedGames = $this->fetchRecentlyReviewedGames($credentials['access_token']);
+            if (!isset($recentlyReviewedGames)) {
+                throw new \Exception('Failed to fetch recently reviewed games.');
+            }
+            $mostAnticipatedGames = $this->fetchMostAnticipatedGames($credentials['access_token']);
+            if (!isset($mostAnticipatedGames)) {
+                throw new \Exception('Failed to fetch most anticipated games.');
+            }
+            $gamesComingSoon = $this->fetchGamesComingSoon($credentials['access_token']);
+            if (!isset($gamesComingSoon)) {
+                throw new \Exception('Failed to fetch games coming soon.');
+            }
+            return view('index', [
+                    'popularGames' => $popularGames,
+                    'recentlyReviewedGames' => $recentlyReviewedGames,
+                    'mostAnticipatedGames' => $mostAnticipatedGames,
+                    'gamesComingSoon' => $gamesComingSoon,
+                ]
+            );
         } catch (\Exception $e) {
             Log::error($e->getMessage());
             return response()->json(['error' => $e->getMessage()], 500);
@@ -45,15 +64,101 @@ class GamesController extends Controller
     }
 
     /**
-     * Fetch popular games from IGDB API using the access token.
+     * Fetch popular games from IGDB API.
      */
-    private function fetchPopularGames($accessToken)
+    private function fetchPopularGames(string $accessToken)
     {
+        $before = Cache::remember('before_timestamp', 60, function () {
+            return Carbon::now()->subMonths(2)->timestamp;
+        });
+        $after = Cache::remember('after_timestamp', 60, function () {
+            return Carbon::now()->addMonths(2)->timestamp;
+        });
         return Http::withHeaders([
             'Client-ID' => config('services.twitch_api.client_id'),
             'Authorization' => 'Bearer ' . $accessToken,
         ])->withBody(
-            'fields name, total_rating_count; sort total_rating_count desc; limit 20;',
+            "fields name, cover.url, first_release_date, total_rating_count, platforms.abbreviation, rating;
+            where platforms = (6,130,167,169)
+            & (first_release_date >= {$before}
+            & first_release_date < {$after})
+            & total_rating_count > 10;
+            sort total_rating_count desc;
+            limit 20;",
+            'text/plain'
+        )->post('https://api.igdb.com/v4/games')->json();
+    }
+
+    /**
+     * Fetch recently reviewed games from IGDB API.
+     */
+    private function fetchRecentlyReviewedGames(string $accessToken)
+    {
+        $before = Cache::remember('before_timestamp', 60, function () {
+            return Carbon::now()->subMonths(2)->timestamp;
+        });
+        $current = Cache::remember('current_timestamp', 60, function () {
+            return Carbon::now()->timestamp;
+        });
+        return Http::withHeaders([
+            'Client-ID' => config('services.twitch_api.client_id'),
+            'Authorization' => 'Bearer ' . $accessToken,
+        ])->withBody(
+            "fields name, cover.url, first_release_date, total_rating_count, platforms.abbreviation, rating, summary;
+            where platforms = (6,130,167,169)
+            & (first_release_date >= {$before}
+            & first_release_date < {$current})
+            & total_rating_count > 10;
+            limit 3;
+            sort total_rating_count desc;",
+            'text/plain'
+        )->post('https://api.igdb.com/v4/games')->json();
+    }
+
+    /**
+     * Fetch most anticipated games from IGDB API.
+     */
+    private function fetchMostAnticipatedGames(string $accessToken)
+    {
+        $current = Cache::remember('current_timestamp', 60, function () {
+            return Carbon::now()->timestamp;
+        });
+        $after = Cache::remember('after_timestamp_4', 60, function () {
+            return Carbon::now()->addMonths(4)->timestamp;
+        });
+        return Http::withHeaders([
+            'Client-ID' => config('services.twitch_api.client_id'),
+            'Authorization' => 'Bearer ' . $accessToken,
+        ])->withBody(
+            "fields name, cover.url, first_release_date, total_rating_count;
+            where platforms = (6,130,167,169)
+            & (first_release_date >= {$current}
+            & first_release_date < {$after})
+            & total_rating_count > 1;
+            limit 4;
+            sort total_rating_count desc;",
+            'text/plain'
+        )->post('https://api.igdb.com/v4/games')->json();
+    }
+
+    /**
+     * Fetch games coming soon from IGDB API.
+     */
+    private function fetchGamesComingSoon(string $accessToken)
+    {
+        $current = Cache::remember('current_timestamp', 60, function () {
+            return Carbon::now()->timestamp;
+        });
+        return Http::withHeaders([
+            'Client-ID' => config('services.twitch_api.client_id'),
+            'Authorization' => 'Bearer ' . $accessToken,
+        ])->withBody(
+            "fields name, cover.url, first_release_date, total_rating_count;
+            where platforms = (6,130,167,169)
+            & first_release_date >= {$current}
+            & total_rating_count > 1;
+            limit 4;
+            sort total_rating_count desc;",
             'text/plain'
         )->post('https://api.igdb.com/v4/games')->json();
     }
