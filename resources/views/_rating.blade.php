13a861813384100e1a5187308ae69ead1e993cd7
Dan LaFreniere <lafreniere.dj@gmail.com>
2024-09-16 20:31:56 -0700

    Fix indentation

--- resources/views/_rating.blade.php
+++ resources/views/_rating.blade.php
@@ -1,33 +1,33 @@
 <script type="module">
 
-var progressBarContainer = document.getElementById('{{ $selector}}')
-var bar = new ProgressBar.Circle(progressBarContainer, {
-color: 'white',
-// This has to be the same size as the maximum width to
-// prevent clipping
-strokeWidth: 6,
-trailWidth: 3,
-trailColor: '#4A5568',
-easing: 'easeInOut',
-duration: 1800,
-text: {
-autoStyleContainer: false,
-},
-svgStyle: {position: 'absolute', top: '0'},
-from: { color: '#48BB78', width: 6 },
-to: { color: '#48BB78', width: 6 },
-// Set default step function for all animate calls
-step: function(state, circle) {
-circle.path.setAttribute('stroke', state.color);
-circle.path.setAttribute('stroke-width', state.width);
+  let progressBarContainer = document.getElementById('{{ $selector}}');
 
-var value = Math.round(circle.value() * 100);
-if (value === 0) {
-circle.setText('0%');
-} else {
-circle.setText(value+'%');
-}
-}
-});
-bar.animate({{ $rating }} / 100);
+  let bar = new ProgressBar.Circle(progressBarContainer, {
+    color: 'white',
+    strokeWidth: 6,
+    trailWidth: 3,
+    trailColor: '#4A5568',
+    easing: 'easeInOut',
+    duration: 1800,
+    text: {
+      autoStyleContainer: false,
+    },
+    svgStyle: {position: 'absolute', top: '0'},
+    from: { color: '#48BB78', width: 6 },
+    to: { color: '#48BB78', width: 6 },
+    // Set default step function for all animate calls
+    step: function(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute('stroke-width', state.width);
+      let value = Math.round(circle.value() * 100);
+      if (value === 0) {
+        circle.setText('0%');
+      } else {
+        circle.setText(value+'%');
+      }
+    }
+  });
+
+  bar.animate({{ $rating }} / 100);
+
 </script>
