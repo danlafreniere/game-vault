24c54833157a0da3dde21a9704c9d0b4c6b3a53e
Dan LaFreniere <lafreniere.dj@gmail.com>
2024-10-19 16:45:52 -0700

    Improve modal UX and add image gallery functionality

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -114,81 +114,109 @@
       <p class="mt-12">{{ $game['summary'] }}</p>
       @endif
       <div class="mt-12" x-data="{ isTrailerModalVisible: false }">
-          @if (!empty($game['trailer']))
-          <button
-              @click="isTrailerModalVisible = true"
-              class="flex bg-blue-500 text-white font-semibold px-4 py-4 hover:bg-blue-600 rounded transition ease-in-out duration-150"
-          >
-              <svg class="w-6 fill-current" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"></path><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path></svg>
-              <span class="ml-2">Play Trailer</span>
-          </button>
-          <template x-if="isTrailerModalVisible">
-              <div
-                  style="background-color: rgba(0, 0, 0, .5);"
-                  class="z-50 fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
-              >
-                  <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
-                      <div class="bg-gray-900 rounded">
-                          <div class="flex justify-end pr-4 pt-2">
-                              <button
-                                  @click="isTrailerModalVisible = false"
-                                  @keydown.escape.window="isTrailerModalVisible = false"
-                                  class="text-3xl leading-none hover:text-gray-300"
-                              >
-                                  &times;
-                              </button>
-                          </div>
-                          <div class="modal-body px-8 py-8">
-                              <div class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
-                                  <p>{{ $game['trailer'] }}</p>
-                                  <iframe width="560" height="315" class="responsive-iframe absolute top-0 left-0 w-full h-full" src="{{$game['trailer']}}" style="border:0;" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                              </div>
-                          </div>
-                      </div>
+        @if (!empty($game['trailer']))
+        <button
+          @click="isTrailerModalVisible = true"
+          class="flex bg-blue-500 text-white font-semibold px-4 py-4 hover:bg-blue-600 rounded transition ease-in-out duration-150">
+          <svg class="w-6 fill-current" viewBox="0 0 24 24">
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path>
+          </svg>
+          <span class="ml-2">Play Trailer</span>
+        </button>
+        <template x-if="isTrailerModalVisible">
+          <div
+            class="z-50 fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto bg-black bg-opacity-50"
+            @click.self="isTrailerModalVisible = false">
+            <div class="container mx-auto lg:max-w-[80%] rounded-lg overflow-y-auto">
+              <div class="bg-gray-900 rounded">
+                <div class="flex justify-end pr-4 pt-2">
+                  <button
+                    @click="isTrailerModalVisible = false"
+                    @keydown.escape.window="isTrailerModalVisible = false"
+                    class="text-3xl leading-none hover:text-gray-300">
+                    &times;
+                  </button>
+                </div>
+                <div class="modal-body px-8 py-8">
+                  <div class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
+                    <p>{{ $game['trailer'] }}</p>
+                    <iframe width="560" height="315" class="responsive-iframe absolute top-0 left-0 w-full h-full" src="{{$game['trailer']}}" style="border:0;" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                   </div>
+                </div>
               </div>
-          </template>
-          @endif
+            </div>
+          </div>
+        </template>
+        @endif
       </div>
     </div>
   </div>
-  <div class="images-container border-b border-gray-800 pb-12 mt-8" x-data="{ isImageModalVisible: false }">
+  <div class="images-container border-b border-gray-800 pb-12 mt-8" x-data="{
+    isImageModalVisible: false,
+    images: {{ Js::from($game['screenshots']) }},
+    currentIndex: 0,
+    isRotating: false,
+    get currentImage() {
+        return this.images[this.currentIndex]?.huge;
+    },
+    rotateImage(direction) {
+        if (this.isRotating) return;
+        this.isRotating = true;
+        this.currentIndex = (this.currentIndex + direction + this.images.length) % this.images.length;
+        this.$nextTick(() => {
+            this.isRotating = false;
+        });
+    }
+  }">
     @if (!empty($game['screenshots']))
     <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Screenshots</h2>
     <div class="images grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mt-8">
-      @foreach ($game['screenshots'] as $screenshot)
+      @foreach ($game['screenshots'] as $index => $screenshot)
       <div>
-        <a href="{{ $screenshot['huge'] }}"
-            @click.prevent="isImageModalVisible = true
-            image='{{ $screenshot['big'] }}'
-        ">
+        <a href="#"
+          @click.prevent="
+                                isImageModalVisible = true;
+                                currentIndex = {{ $index }};
+                            ">
           <img src="{{ $screenshot['big'] }}" alt="screenshot" class="hover:opacity-75 transition ease-in-out duration-150">
         </a>
       </div>
       @endforeach
     </div>
     <template x-if="isImageModalVisible">
-        <div
-            style="background-color: rgba(0, 0, 0, .5);"
-            class="z-50 fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
-        >
-            <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto w-auto">
-                <div class="bg-gray-900 rounded">
-                    <div class="flex justify-end pr-4 pt-2">
-                        <button
-                            class="text-3xl leading-none hover:text-gray-300"
-                            @click="isImageModalVisible = false"
-                            @keydown.escape.window="isImageModalVisible = false"
-                        >
-                            &times;
-                        </button>
-                    </div>
-                    <div class="modal-body px-8 py-8">
-                        <img :src="image" alt="screenshot">
-                    </div>
-                </div>
+      <div
+        class="z-50 fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50"
+        @click.self="isImageModalVisible = false"
+        @keydown.escape.window="isImageModalVisible = false"
+        @keydown.window.left="rotateImage(-1)"
+        @keydown.window.right="rotateImage(1)">
+        <div class="bg-gray-900 rounded-lg overflow-hidden max-w-3xl max-h-full">
+          <div class="relative">
+            <div class="absolute flex flex-row-reverse items-center text-white p-4 right-0">
+              <button
+                @click="isImageModalVisible = false"
+                class="text-3xl leading-none hover:text-gray-300">
+                &times;
+              </button>
             </div>
+            <img :src="currentImage" alt="screenshot" class="max-w-full max-h-[80vh] object-contain">
+            <button
+              @click="rotateImage(-1)"
+              class="absolute left-0 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 hover:bg-opacity-75">
+              &#10094;
+            </button>
+            <button
+              @click="rotateImage(1)"
+              class="absolute right-0 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 hover:bg-opacity-75">
+              &#10095;
+            </button>
+            <div class="text-white text-center mt-2 absolute w-full bottom-0 opacity-50">
+              Image <span x-text="currentIndex + 1"></span> of <span x-text="images.length"></span>
+            </div>
+          </div>
         </div>
+      </div>
     </template>
     @endif
   </div>
@@ -204,6 +232,5 @@
     </div>
   </div>
   @endif
-</div>
 </div>
 @endsection
